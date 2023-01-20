c62390ad52d4e48794a8d984ace7121a5cf7de3f
HarshithaS98 <harshi.s98@gmail.com>
2023-01-20 14:42:01 +0800

    Specflow Scenarios

---
+++ MarsUpdated/StepDefinitions/CreateSkillStepDefinition.cs
@@ -0,0 +1,25 @@
+﻿
+namespace MarsUpdated;
+[Binding]
+public class CreateSkillStepDefinition
+{
+    CreateSkill ASP;
+
+    public CreateSkillStepDefinition()
+    {
+        ASP= new CreateSkill();    
+    }
+
+    [When(@"User adds a '([^']*)' and a '([^']*)'")]
+    public void WhenUserAddsANewSkillAndSkillLevel(string skill, string skillLevel)
+    {
+        ASP.CreateSkills(skill,skillLevel);
+    }
+
+    [Then(@"'([^']*)' and '([^']*)' should be added successfully")]
+    public void ThenNewSkillShouldBeAddedSuccessfully(string skill, string skillLevel)
+    {
+        ASP.CheckSkillAdded(skill,skillLevel);
+    }
+
+}
---
+++ MarsUpdated/StepDefinitions/CreateanguageStepDefinitions.cs
@@ -0,0 +1,33 @@
+﻿using MarsUpdated.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsUpdated;
+[Binding]
+
+    public class CreateanguageStepDefinitions : CommonDriver
+    {
+         CreateLanguage ASP;
+
+        public CreateanguageStepDefinitions()
+        {
+            ASP = new CreateLanguage();
+        }
+
+    [When(@"User adds a '([^']*)' and  '([^']*)'")]
+    public void WhenUserAddsAAnd(string Language, string Level)
+    {
+        throw new PendingStepException();
+    }
+
+    [Then(@"'([^']*)' and '([^']*)'   language created sucessfully")]
+    public void ThenAndLanguageCreatedSucessfully(string Language, string Level)
+    {
+        throw new PendingStepException();
+    }
+
+}
+
