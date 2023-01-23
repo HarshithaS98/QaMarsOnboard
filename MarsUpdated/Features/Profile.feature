f8d5f432e5ee5cb90bc74ca9a611424a2eda246a
HarshithaS98 <harshi.s98@gmail.com>
2023-01-23 13:37:35 +0800

    Scenario 3 Adding certifications

--- MarsUpdated/Features/Profile.feature
+++ MarsUpdated/Features/Profile.feature
@@ -20,8 +20,84 @@ Scenario Outline: create new Skill
     Scenario Outline: Create new Language
     When User adds a '<Language>' and  '<Level>'
     Then  '<Language>' and '<Level>'   language created sucessfully    
+    
     Examples: 
     | Language | Level          |
     | English  | Fluent         |
     | Mandarin | Conversational |
     
+    Scenario Outline: User create certifications using certification tab
+    When Added '<Certificate>' and '<From>' and '<Year>'  to the Certifications
+    Then   '<certificate>' and '<From>' and '<Year>'  created successfully
+    Examples: 
+    | Certificate | From | Year    |
+    | ISTQB       | SGTQB | 2022    |
+    | API         | INDUSTRYCONNECT | 2023|
+
+    
+   #Scenario Outline: User edited existing certificate details with valid details
+   #Given edited '<certificate>' and '<Year>' and '<From>'  to the Certifications
+   #Then the '<certificate>' and '<Year>' and '<From>'  to the Certifications edited successfully
+
+ #Examples: 
+ #| certificate  | From             | Year | 
+  #Scenario: User able to delete edited certificate details  with valid details
+  #Given Deleted '<certificate>', '<Year>' , '<From>'  to the Certifications
+  #Then the '<certificate>' , '<Year>' , '<From>'  to the Certifications  deleted successfully
+
+
+ #All The Testcases for profile page
+#Scenario:Verify validation error for certification
+#When Lefted '<certificate>' and <Year> and <From>  to the Certificationsnot choosen or when duplicate data exists
+#Then error message should be displayed
+
+#Scenario Outline: User writes  description using description tab
+#When When Added <description>  to the description
+#Then the <description>   created successfully
+
+ #Examples: 
+ #| Description                                                            |  
+ #| Iam Harshitha, I would like to say that Iam a Qa Intern at MvP studios |
+
+
+#Scenario Outline: User edited existing description details with valid details
+#When edited<description>  to the description
+#Then the<description>  to the description edited successfully
+
+ #Examples: 
+ #| Description                                                                      |
+ #| Iam Harshitha, I would like to say that Iam a Test analyst Intern at MvP studios |
+
+#Scenario: User able to delete edited description details
+#When Deleted <description>  to the description
+#Then the <description>  to the description deleted successfully
+
+#Scenario:Verify validation error for description
+#When Lefted  <description> not choosen or when duplicate data exists
+#Then error message should be displayed
+
+
+#Scenario Outline: User create Education using education tab
+#When When Added <country> and <University> and <Title> <Degree> <Graduation year> to the education
+#Then the <country> and <University> and <Title> <Degree> <Graduation year> created successfully
+
+ #Examples: 
+ #| country | University | Title | Degree    | Graduation year |
+# | India   | JNTU       | Btech | Bachelors | 2013            |
+
+
+#Scenario Outline: User edited existing education details with valid details
+#When edited<country> and <University> and <Title> <Degree> <Graduation year> to the education
+#Then the<country> <University>  <Title> <Degree> <Graduation year>  edited successfully
+
+#Examples: 
+#| country   | University          | Title | Degree  | Graduation year |
+#| Singapore | National University | MS    | Masters | 2020            |
+
+#Scenario: User able to delete edited education  with valid details
+#When Deleted <country> and <University> and <Title> <Degree> <Graduation year> to the education
+#Then the <country> <University> <Title> <Degree> <Graduation year>  deleted successfully
+
+#Scenario:Verify validation error for education
+#When Lefted <country> and <University> and <Title> <Degree> <Graduation year> not choosen or when duplicate data exists
+#Then error message should be displayed
---
+++ MarsUpdated/Pages/CreateCertification.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsUpdated.Pages
+{
+    public class CreateCertification : CommonDriver
+
+    {
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        public void CreateCertifications(string Certificate, string From, string Year)
+        {
+
+            // Identify Certification button and click and using explicit waits to locate elements
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[4]")));
+            IWebElement cerbutton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[4]"));
+            cerbutton.Click();
+
+            //    // identify add new button
+            wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/table/thead/tr/th[4]/div")));
+            IWebElement addCer = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/table/thead/tr/th[4]/div"));
+            addCer.Click();
+
+            // Add certifications to certification tab
+            IWebElement certab = driver.FindElement(By.Name("certificationName"));
+            certab.SendKeys(Certificate);
+
+            // Add certified 'From" toolbox
+            IWebElement fromtab = driver.FindElement(By.Name("certificationFrom"));
+            certab.SendKeys(From);
+
+
+            // select certified "Year" dropdown
+            SelectElement yeartab = new SelectElement(driver.FindElement(By.Name("certificationYear")));
+            yeartab.SelectByValue(Year);
+
+            // Identify Add button and click
+            wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/div/div[3]/input[1]")));
+            IWebElement Addcer = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/div/div[3]/input[1]"));
+            Addcer.Click();
+        }
+
+         public void CheckCertificationAdded(string Certificate, string From, string Year)
+            
+         {
+                //boolean value to check if the skill is added for assertion
+                bool CertificationAdded = false;
+                //reading all the columns from the table
+                ReadOnlyCollection<IWebElement> elements;
+                wait.Until(ExpectedConditions.ElementIsVisible(By.TagName("td")));
+                elements = driver.FindElements(By.TagName("td"));
+                for (int i = 0; i < elements.Count; i++)
+                {
+                    //checking to see if the value of skill is added to any column in the table
+                    if (elements[i].Text == Certificate)
+                    {
+                    //true if skill present
+                     CertificationAdded = true;
+                        break;
+                    }
+                }
+
+
+
+         }
+    } 
+}
+
+
+
+                //fluent assertions. True if skill added
+               // skillAdded.Should().BeTrue();
+                //public void CheckSkillAdded(string skill, string skillLevel)
+                //{
+                //    //boolean value to check if the skill is added for assertion
+                //    bool skillAdded = false;
+                //    //reading all the columns from the table
+                //    ReadOnlyCollection<IWebElement> elements;
+                //    wait.Until(ExpectedConditions.ElementIsVisible(By.TagName("td")));
+                //    elements = driver.FindElements(By.TagName("td"));
+                //    for (int i = 0; i < elements.Count; i++)
+                //    {
+                //        //checking to see if the value of skill is added to any column in the table
+                //        if (elements[i].Text == skill)
+                //        {
+                //            //true if skill present
+                //            skillAdded = true;
+                //            break;
+                //        }
+                //    }
+                //    //fluent assertions. True if skill added
+                //    skillAdded.Should().BeTrue();
+
+
+
+
+
+
+
+
+
+
+
+            }
+        //public void CreateCertification()
+        ////Identify certification button
+        //IWebElement cerbutton = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[1]/a[4]"));
+        //cerbutton.Click();
+        //    // identify add new button
+        //    IWebElement addCer = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/table/thead/tr/th[4]/div"));
+        //addCer.Click();
+
+        //    //identify certificate or award textbox and add Istqb Certification
+        //    IWebElement certoolbox = driver.FindElement(By.Name("certificationName"));
+        //certoolbox.SendKeys(From);
+        //    // identify year dropbox and choose 2022
+        //    IWebElement yearCer = driver.FindElement(By.Name("certificationYear"));
+        ////choose2022
+        //yearCer.Click();
+        //    IWebElement yearofcer = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/div/div[2]/div[2]/select/option[3]"));
+        //yearofcer.Click();
+        //    //identify add and save details
+        //    IWebElement addcertificate = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/div/div[3]/input[1]"));
+       
